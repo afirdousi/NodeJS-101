c46524314a5298accc89209ecf169bbd08eb1310
Anas Raza Firdousi <anas.firdousi@gmail.com>
2015-10-18 00:49:23 -0700

    Remove directory Node-API-Security-With-JWT and renamed it to 'jwt'

--- Node-API-Security-With-JWT/001-basic-implementation/app/config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Anas on 10/17/2015.
- */
-module.exports ={
-
-    "secret":"JWTSECRETTOKEN123"
-}
--- Node-API-Security-With-JWT/001-basic-implementation/app/routes.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Anas on 10/17/2015.
- */
-
-/*In this basic implementation project, all routes are within server.js*/
